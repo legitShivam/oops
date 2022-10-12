9da3b14e1ebaa590e2a5cc3c65acda832fca6c99
Shivam Shandilya <70915754+legitShivam@users.noreply.github.com>
2022-10-12 10:32:58 +0530

    Update operator2.cpp

--- operator2.cpp
+++ operator2.cpp
@@ -2,4 +2,46 @@
 // shop and processes objects of these classes using pointers to base class.  Write the rules of virtual functions.9.Create two objects of class string s1and s2 as shown below:String S1= "Amity"String S2= "University"Implement the following operations and write the output of each operation:Copy the content of S1 to S3Find the substring Univ in S2Compare S1 and S210.Write a program to implement stack functions using templates.Open ended Question1.Write a program that input a file, which determines its length. Also count the number of word occurrence. For example:” that person is going to town to meet other person”. Here “to” and “person”-2times.2.Write a program to demonstrate exception handling.3.Write a program to implement a singleton class.
 
 // C++ Programming Assignment Help
+#include <iostream>
+using namespace std;
+
+
+class media{
+    private:
+        string title;
+        string publication;
+    public:
+        virtual void read(){}
+        virtual void show(){}
+};
+
+class books: public media{
+    private:
+        int pages;
+    public:
+        void read(){
+            cout << "Reading a book" << endl;
+        }
+
+        void show(){
+            cout << "Showing a book" << endl;
+        }
+};
+
+class videoTape: public media{
+    private:
+        struct time{
+            int hours;
+            int minutes;
+        };
+        struct time playing_time;
+    public:
+        void read(){
+            cout << "Reading a video tape" << endl;
+        }
+        
+        void show(){
+            cout << "Showing a video tape" << endl;
+        }
+};
 
